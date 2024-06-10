27516c1bab0345c04514b2d41924baa8636987a5
oswaldoavalos9 <oswaldoale12357@gmail.com>
2024-06-10 20:55:43 +0100

    POO_2024

--- parcial1/3_entradas_salidas_datos/entrada_salida.py
+++ parcial1/3_entradas_salidas_datos/entrada_salida.py
@@ -1,26 +1,23 @@
-<<<<<<< HEAD
 #Solicitar los datos de un alumno
 
 nombre=input("Nombre")
 edad=input("Edad")
 estatura=input("Estatura")
 
-=======
-<<<<<<< HEAD
+
 #Solicitar los datos de un alumno
 
 nombre=input("Nombre")
 edad=input("Edad")
 estatura=input("Estatura")
 
-=======
+
 #Solicitar los datos de un alumno
 
 nombre=input("Nombre")
 edad=input("Edad")
 estatura=input("Estatura")
+print("hola")
 
->>>>>>> 25859170a0c301bb74334622a8104cfe9d1ef047
->>>>>>> ddb1045bbb29ea5fca236c962e8dd2c8efc2216f
 print(f"/t ..::::Datos del alumno:::../n Nombre: {nombre} /n Edad: {edad} /n Estatura: {estatura}") 
 print("")
