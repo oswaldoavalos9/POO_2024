6537efd25228663a70b1bdfa906acfd702cd01d8
oswaldoavalos9 <oswaldoale12357@gmail.com>
2024-06-26 16:39:24 +0100

    ER Parcial2 POO of VSCode

---
+++ parcial2/4_constructor/main.py
@@ -0,0 +1,13 @@
+from coches import
+
+print("objeto 1")
+coche1 = Coches("blanco","vw","2022",220,150,5)
+
+
+coche1.getInfo()
+
+
+print("objeto 2")
+coche2 = coches("azul","nissan","2020",180,150,6)
+coche2.getInfo()
+
---
+++ parcial2/5_encapsulamiento/main.py
@@ -0,0 +1,23 @@
+from coches import *
+
+print("Objeto 1")
+coche1=Coches("Blanco","VW","2022",220,150,5)
+
+# coche1.getInfo()
+
+
+# print("Objeto 2")
+# coche2=Coches("Azul","Nissan","2020",180,150,6)
+# coche2.getInfo()
+
+
+
+#implementar el encapsulamiento o visibilidad
+
+print(coche1.atributo_publico)
+# print(coche1.__atributo_privado) #No es posible
+print(coche1.MetodoPublico())
+
+
+# coche1.__MetodoPrivado() #No es posible
+coche1.getMetodoPublico()
